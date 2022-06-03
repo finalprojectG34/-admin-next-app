4f93bf58697583235c89542c4cce87ee4e71f8cd
bekzemed <bereketzemed@gmail.com>
2022-06-03 23:27:35 +0300

    user update page created

--- pages/user/user-list/user-list.jsx
+++ pages/user/user-list/user-list.jsx
@@ -1,100 +1,149 @@
-import {useLazyQuery, useMutation} from '@apollo/client';
+import { useLazyQuery, useMutation, useQuery } from '@apollo/client'
 
 import {
-    Alert,
-    Paper,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TableHead,
-    TableRow,
-    Typography
-} from '@mui/material';
-import {HighlightOffOutlined} from '@mui/icons-material';
+  Alert,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material'
+import { HighlightOffOutlined, CachedOutlined } from '@mui/icons-material'
 
-import MainCard from '../../../src/ui-components/cards/MainCard';
-import Loader from "../../../src/ui-components/Loader";
+import MainCard from '../../../src/ui-components/cards/MainCard'
+import Loader from '../../../src/ui-components/Loader'
 
-import {GET_ALL_USERS} from "../../../src/apollo/queries/user_queries";
-import {DELETE_USER} from "../../../src/apollo/mutations/user_mutation";
+import {
+  GET_ALL_USERS,
+  GET_ONE_USER,
+} from '../../../src/apollo/queries/user_queries'
+import { DELETE_USER } from '../../../src/apollo/mutations/user_mutation'
 
-import {useEffect, useState} from 'react';
-
+import { useEffect, useState } from 'react'
+import UserUpdate from '../../../src/ui-components/update-cards/UserUpdate'
 
 const UserList = () => {
-    const [getUsers, {data, error, loading}] = useLazyQuery(GET_ALL_USERS, {
-        nextFetchPolicy: "network-only"
-    });
-    const [deleteUser] = useMutation(DELETE_USER);
+  const [getUsers, { data, error, loading }] = useLazyQuery(GET_ALL_USERS, {
+    nextFetchPolicy: 'network-only',
+  })
 
-    useEffect(() => {
-        getUsers().then((d) => {
-            console.log(d)
-        })
-    }, [])
+  const [deleteUser] = useMutation(DELETE_USER)
 
-    if (error)
-        return (
-            <Alert variant='outlined' severity='error'>
-                {error.message}
-            </Alert>
-        );
-    if (loading) return <Loader/>;
+  const [open, setOpen] = useState(false)
+  const [currentUserId, setCurrentUserId] = useState('')
+
+  const { data: getOneUserData } = useQuery(GET_ONE_USER, {
+    variables: { getUserByIdId: currentUserId },
+  })
+
+  useEffect(() => {
+    getUsers().then((d) => {
+      console.log(d)
+    })
+  }, [])
+
+  const setUserUpdate = (id) => {
+    setOpen(true)
+    setCurrentUserId(id)
+  }
+
+  if (error)
     return (
-        <MainCard title='User List' sx={{margin: 'auto'}} style={{maxWidth: 'max-content'}}>
-            <Typography variant='body2' component="div">
-                <TableContainer component={Paper}>
-                    <Table sx={{minWidth: 650, bgcolor: '#00000021'}}>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Number</TableCell>
-                                <TableCell align='right'>First Name</TableCell>
-                                <TableCell align='right'>Last Name</TableCell>
-                                <TableCell align='right'>Email</TableCell>
-                                <TableCell align='right'>Phone Number</TableCell>
-                                <TableCell align='right'>Delete</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {data?.getAllUsers?.map((user, index) =>
-                                !user ? null : (
-                                    <TableRow
-                                        key={user.id}
-                                        sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                                        data-cy='user-list-element'
-                                    >
-                                        <TableCell component='th' scope='row'>{index + 1}</TableCell>
-                                        <TableCell align='right'>{user.firstName}</TableCell>
-                                        <TableCell align='right'>{user.lastName}</TableCell>
-                                        <TableCell align='right'>{user.email}</TableCell>
-                                        <TableCell align='right'>{user.phone}</TableCell>
-                                        <TableCell align='right'>
-                                            <HighlightOffOutlined
-                                                data-cy='user-delete-element'
-                                                onClick={() => {
-                                                    deleteUser({
-                                                        variables: {
-                                                            deleteUserId: user.id
-                                                        },
-                                                        update: cache => {
-                                                            cache.evict({id: 'User:' + user.id});
-                                                        }
-                                                    }).then(() => {
+      <Alert variant='outlined' severity='error'>
+        {error.message}
+      </Alert>
+    )
+  if (loading) return <Loader />
+  return (
+    <MainCard
+      title='User List'
+      sx={{ margin: 'auto' }}
+      style={{ maxWidth: 'max-content' }}
+    >
+      {open && (
+        <UserUpdate
+          handleClose={setOpen}
+          open={open}
+          data={getOneUserData?.getUserById}
+        />
+      )}
+      <Typography variant='body2' component='div'>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650, bgcolor: '#00000021' }}>
+            <TableHead>
+              <TableRow>
+                <TableCell>Number</TableCell>
+                <TableCell align='right'>First Name</TableCell>
+                <TableCell align='right'>Last Name</TableCell>
+                <TableCell align='right'>Phone Number</TableCell>
+                <TableCell align='right'>Delete</TableCell>
+                <TableCell align='right'>Edit</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data?.getAllUsers?.map((user, index) =>
+                !user ? null : (
+                  <TableRow
+                    key={user.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    data-cy='user-list-element'
+                  >
+                    <TableCell component='th' scope='row'>
+                      {index + 1}
+                    </TableCell>
+                    <TableCell align='right'>{user.firstName}</TableCell>
+                    <TableCell align='right'>{user.lastName}</TableCell>
+                    <TableCell align='right'>{user.phone}</TableCell>
+                    <TableCell align='right'>
+                      <HighlightOffOutlined
+                        data-cy='user-delete-element'
+                        onClick={() => {
+                          deleteUser({
+                            variables: {
+                              deleteUserId: user.id,
+                            },
+                            update: (cache) => {
+                              cache.evict({ id: 'User:' + user.id })
+                            },
+                          }).then(() => {})
+                        }}
+                        color='error'
+                        sx={{ cursor: 'pointer' }}
+                        fontSize='small'
+                      />
+                    </TableCell>
+                    <TableCell align='right'>
+                      <CachedOutlined
+                        data-cy='user-edit-element'
+                        onClick={() => setUserUpdate(user.id)}
+                        // onClick={() =>
+                        //     {
+                        //   updateUser({
+                        //     variables: {
+                        //       deleteUserId: user.id,
+                        //     },
+                        //     update: (cache) => {
+                        //       cache.evict({ id: 'User:' + user.id })
+                        //     },
+                        //   }).then(() => {})
+                        // }}
+                        color='success'
+                        sx={{ cursor: 'pointer' }}
+                        fontSize='small'
+                      />
+                    </TableCell>
+                  </TableRow>
+                )
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Typography>
+    </MainCard>
+  )
+}
 
-                                                    });
-                                                }}
-                                                color='error' sx={{cursor: 'pointer'}} fontSize="small"/>
-                                        </TableCell>
-                                    </TableRow>
-                                )
-                            )}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-            </Typography>
-        </MainCard>
-    );
-};
-
-export default UserList;
+export default UserList
---
+++ src/ui-components/update-cards/UserUpdate.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import {
+  Box,
+  Button,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+} from '@mui/material'
+import { GET_ONE_USER } from '../../apollo/queries/user_queries'
+import { useQuery, useMutation } from '@apollo/client'
+import { InputField } from '../input/InputField'
+import { UPDATE_USER } from '../../apollo/mutations/user_mutation'
+
+const UserUpdate = ({ handleClose, open, data }) => {
+  // const { data, loading } = useQuery(GET_ONE_USER, {
+  //   variables: { getUserByIdId: id },
+  // })
+  if (!data) {
+    return
+  }
+
+  const [firstName, setFirstName] = useState(data.firstName || '')
+  const [lastName, setLastName] = useState(data.lastName || '')
+  const [phone, setPhone] = useState(data.phone || '')
+  const [updateUser] = useMutation(UPDATE_USER)
+
+  const update = () => {
+    // console.log({ firstName, lastName, email, phone, data.id })s
+    updateUser({
+      variables: {
+        input: { firstName, lastName, email, phone },
+        updateUserId: data.id,
+      },
+    })
+    handleClose()
+  }
+  const closeModal = () => {
+    handleClose()
+  }
+  return (
+    // <Box>
+    <Dialog open={open} onClose={closeModal}>
+      <DialogTitle fontSize={18} textAlign={'center'}>
+        User Update
+      </DialogTitle>
+      <DialogContent>
+        <InputField
+          value={firstName}
+          label='firstName'
+          type='text'
+          name='firstName'
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <InputField
+          value={lastName}
+          label='lastName'
+          type='text'
+          name='lastName'
+          onChange={(e) => setLastName(e.target.value)}
+        />
+
+        <InputField
+          value={phone}
+          label='phone'
+          name='phone'
+          onChange={(e) => setPhone(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => closeModal()}>Cancel</Button>
+        <Button onClick={() => update()}>Update</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default UserUpdate
